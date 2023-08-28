9daa8dda47a9f36d2834c312fe6ad94f1670224c
Marzdevs <alzokarimarwa@gmail.com>
2023-08-28 15:54:56 -0500

    next.js starter

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() { 
+  return (
+    <div className="bg-blue-900 w-screen h-screen flex items-center"> 
+    <div className="text-center w-full">
+    <button className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
+    </div>
+    </div>
+  )
+}
