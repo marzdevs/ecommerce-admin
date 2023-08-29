f126eba27227a6b98675119e64a513dc767496c6
Marzdevs <alzokarimarwa@gmail.com>
2023-08-28 20:14:28 -0500

    fixed mongodb issues

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,26 +1,15 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
-
+import { MongoDBAdapter } from '@auth/mongodb-adapter';
+import clientPromise from "@/lib/mongodb";
 
 export default NextAuth({
   providers: [
     // OAuth authentication providers...
-    AppleProvider({
-      clientId: process.env.APPLE_ID,
-      clientSecret: process.env.APPLE_SECRET
-    }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET
-    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET
     }),
-    // Passwordless / email sign in
-    EmailProvider({
-      server: process.env.MAIL_SERVER,
-      from: 'NextAuth.js <no-reply@example.com>'
-    }),
-  ]
+  ],
+  adapter: MongoDBAdapter(clientPromise),
 })
